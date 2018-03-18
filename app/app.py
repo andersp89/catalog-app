006193196a1d8f24df1c633df0200f85475b945d
andersp89 <anders@ptrading.dk>
2018-03-18 15:24:35 +0100

    test wsgi app

--- app/app.py
+++ app/app.py
@@ -331,6 +331,7 @@ def singleItemJSON(category_name, category_id, item_name, item_id):
     return jsonify(item=item.serialize)
 
 if __name__ == '__main__':
+    print "hallo med dig"
     app.secret_key = "secret_in_production123"  # SECRET IN PRODUCTION!
     app.debug = True
     app.run(host='18.195.163.63', port=80)
