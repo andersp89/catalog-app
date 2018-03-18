b96239ec62b27f6d65506e0937179ce445c10f0c
andersp89 <anders@ptrading.dk>
2018-03-18 12:14:59 +0100

    wsgi support test

--- app/app.py
+++ app/app.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
+
+def application(environ, start_response):
+    status = '200 OK'
+    output = 'Hello Udacity!'
+
+    response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
+
+
+
 # Framework and DB
 from flask import Flask, render_template, redirect, url_for, request, flash, \
     jsonify
